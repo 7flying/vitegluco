491809551e0361756674d3ee3016d4bdeec06934
7flying <diez.f.irene@gmail.com>
2015-04-13 22:58:50 +0200

    Add more skeleton files

--- vitegluco/app/run.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-from app.vitegluco import app as app
-
-app.run(host='0.0.0.0', debug=True, port=5000)
---
+++ vitegluco/run.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from app.vitegluco import app as app
+
+app.run(host='0.0.0.0', debug=True, port=5000)
